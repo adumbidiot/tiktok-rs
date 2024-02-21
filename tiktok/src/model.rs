8039f4b3113cd40807e4f559aa8a00ffee28787f
Nathaniel Daniel <nathaniel.daniel23@outlook.com>
2024-02-20 22:08:55 -0800

    Remove scraping functionality (#25)
    * Remove scraping functions
    
    TikTok changed the format again, and it is very different from before. Instead of focusing on scraping, which breaks easily, we will focus on incorporating app functionality.
    
    * Fix deny

--- tiktok/src/model.rs
+++ tiktok/src/model.rs
@@ -1,7 +1,4 @@
 mod feed;
-mod scraped_post_page;
 
 pub use self::feed::FeedCursor;
 pub use self::feed::Post;
-pub use self::scraped_post_page::FromHtmlError as InvalidScrapedPostPageError;
-pub use self::scraped_post_page::ScrapedPostPage;
