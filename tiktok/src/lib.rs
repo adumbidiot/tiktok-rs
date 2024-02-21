b4cbe293acb47b6cfa86ffc455da7fb83626cf4b
Nathaniel Daniel <nathaniel.daniel23@outlook.com>
2024-02-20 21:19:55 -0800

    Remove scraping functions
    TikTok changed the format again, and it is very different from before. Instead of focusing on scraping, which breaks easily, we will focus on incorporating app functionality.

--- tiktok/src/lib.rs
+++ tiktok/src/lib.rs
@@ -3,9 +3,7 @@ mod model;
 
 pub use self::client::Client;
 pub use self::model::FeedCursor;
-pub use self::model::InvalidScrapedPostPageError;
 pub use self::model::Post;
-pub use self::model::ScrapedPostPage;
 pub use url::Url;
 
 #[derive(Debug, thiserror::Error)]
@@ -17,10 +15,6 @@ pub enum Error {
     /// A Tokio task failed to join
     #[error(transparent)]
     TokioJoin(#[from] tokio::task::JoinError),
-
-    /// Failed to parse a [`ScrapedPostPage`]
-    #[error("invalid post page")]
-    InvalidScrapedPostPage(#[from] InvalidScrapedPostPageError),
 }
 
 #[cfg(test)]
@@ -33,21 +27,6 @@ mod test {
     // "https://vm.tiktok.com/TTPdrksrdc/"
     // "https://www.tiktok.com/t/ZTRQsJaw1/"
     const POST_URLS: &[&str] = &["https://www.tiktok.com/@von.jakoba/video/7270331232595021098"];
-
-    #[tokio::test]
-    async fn scrape_post() {
-        let client = Client::new();
-        for url in POST_URLS {
-            let post = client.scrape_post(url).await.expect("failed to get post");
-            let _item_id = post
-                .sigi_state
-                .item_module
-                .posts
-                .keys()
-                .next()
-                .expect("missing item_id");
-        }
-    }
 
     #[tokio::test]
     async fn get_feed_post() {
