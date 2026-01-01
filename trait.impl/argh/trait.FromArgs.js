4d2f9fb0e451f1bf55e29205b89a576df725bc96
adumbidiot <adumbidiot@users.noreply.github.com>
2026-01-01 10:09:05 +0000

    deploy: 824cf9543c92b8fd77237df69c0079ac49945507

---
+++ trait.impl/argh/trait.FromArgs.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["tiktok_cli",[["impl FromArgs for <a class=\"struct\" href=\"tiktok_cli/struct.Options.html\" title=\"struct tiktok_cli::Options\">Options</a>"]]],["tikwm_cli",[["impl FromArgs for <a class=\"struct\" href=\"tikwm_cli/struct.Options.html\" title=\"struct tikwm_cli::Options\">Options</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[147,145]}
---
+++ trait.impl/serde_core/de/trait.Deserialize.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["tiktok",[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/de/trait.Deserialize.html\" title=\"trait serde_core::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"tiktok/struct.FeedCursor.html\" title=\"struct tiktok::FeedCursor\">FeedCursor</a>"],["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/de/trait.Deserialize.html\" title=\"trait serde_core::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"tiktok/struct.Post.html\" title=\"struct tiktok::Post\">Post</a>"]]],["tikwm",[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/de/trait.Deserialize.html\" title=\"trait serde_core::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"tikwm/model/struct.GetTaskResultResponse.html\" title=\"struct tikwm::model::GetTaskResultResponse\">GetTaskResultResponse</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[604,361]}
